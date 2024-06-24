e3f33611af9608b3d2cdaa43efdcdcedeac7210b
Lanteriso <214778990@qq.com>
2024-06-24 20:59:26 +0800

    2024.6.25

--- DemoC1.py
+++ DemoC1.py
@@ -1,7 +1,7 @@
 import socket
 
 class EchoClient:
-    def __init__(self, host='127.0.0.1', port=12345):
+    def __init__(self, host='192.168.0.105', port=12345):
         self.host = host
         self.port = port
         self.client_socket = None
